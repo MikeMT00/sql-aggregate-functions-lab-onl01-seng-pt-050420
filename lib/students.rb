9c7990fab6c229997e1411ef74de02b7ea8b7d1d
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-04 18:06:23 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SLECT SUM(tardies) FROM students WHERE"
+  "SLECT SUM(tardies) AS average_gpa FROM students"
 end
 
 def average_gpa_for_9th_grade
